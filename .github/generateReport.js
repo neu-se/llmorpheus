48793bcbbbedd68c855962bedd660f8703b9f7c5
franktip <f.tip@northeastern.edu>
2024-12-19 12:44:26 -0500

    update report with number of retries and failures

--- .github/generateReport.js
+++ .github/generateReport.js
@@ -54,9 +54,13 @@ function generateLLMorpheusReport(title, dirName, mutantsDirName){
     totalCompletionTokens += nrCompletionTokens;
     const nrTotalTokens = llmJsonObj.totalTokens;
     totalTotalTokens += nrTotalTokens;
-    report += `| ${benchmark} | ${nrPrompts} | ${nrTotal} | ${nrKilled} | ${nrSurvived} | ${nrTimedOut} | ${mutationScore} | ${llmorpheusTime} | ${strykerTime} | ${nrPromptTokens} | ${nrCompletionTokens} | ${nrTotalTokens} |\n`;
+    nrRetries = llmJsonObj.nrRetries;
+    totalNrRetries += nrRetries;
+    nrFailures = llmJsonObj.nrFailures;
+    totalNrFailures += nrFailures;
+    report += `| ${benchmark} | ${nrPrompts} | ${nrTotal} | ${nrKilled} | ${nrSurvived} | ${nrTimedOut} | ${mutationScore} | ${llmorpheusTime} | ${strykerTime} | ${nrPromptTokens} | ${nrCompletionTokens} | ${nrTotalTokens} | ${nrRetries} | ${nrFailures} |\n`;
   } 
-  report += `| Total | ${totalPrompts} | ${totalMutants} | ${totalKilled} | ${totalSurvived} | ${totalTimedOut} | - | ${totalLLMorpheusTime.toFixed(2)} | ${totalStrykerTime.toFixed(2)} | ${totalPromptTokens} | ${totalCompletionTokens} | ${totalTotalTokens} |\n`;
+  report += `| Total | ${totalPrompts} | ${totalMutants} | ${totalKilled} | ${totalSurvived} | ${totalTimedOut} | - | ${totalLLMorpheusTime.toFixed(2)} | ${totalStrykerTime.toFixed(2)} | ${totalPromptTokens} | ${totalCompletionTokens} | ${totalTotalTokens} | ${totalNrRetries} | ${totalNrFailures} |\n`;
 
   const metaData = retrieveMetaData(mutantsDirName);
   
