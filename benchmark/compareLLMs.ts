4e86b81f0e51608c02e1368605b3022e1f4d522c
franktip <f.tip@northeastern.edu>
2024-12-25 09:32:29 -0500

    update scripts for generating latex tables

--- benchmark/compareLLMs.ts
+++ benchmark/compareLLMs.ts
@@ -17,62 +17,176 @@ const projectNames = [
   "zip-a-folder",
 ];
 
-/**
- * Generate a LaTeX table that shows the number of mutants generated
- * at different temperatures.
- */
-function generateTable(baseDir: string, runs: string[]): void {
-  let latexTable =
-    `% table generated using command: "node benchmark/computeVariability.js ${baseDir.substring(
-      baseDir.lastIndexOf("/") + 1
-    )}${runs.join(" ")}"\n` +
-    "\\begin{table*}\n" +
-    "\\centering\n" +
-    "{\\scriptsize\n" +
-    "\\begin{tabular}{l||cccc|cccc|cccc}\n" +
-    "        & \\multicolumn{4}{|c|}{\\bf \\CodeLlamaThirtyFour} &  \\multicolumn{4}{|c|}{\\bf \\CodeLlamaThirteen} &   \\multicolumn{4}{|c}{\\bf \\Mixtral} \\\\\n" +
-    "                      &  \\Total & \\Killed & \\Survived & \\Timeout \n" +
-    "                      &  \\Total & \\Killed & \\Survived & \\Timeout  \n" +
-    "                      &  \\Total & \\Killed & \\Survived & \\Timeout  \\\\\n" +
-    "\\hline\n" +
-    "\\hline\n";
+/** Format a number with commas to separate thousands and millions, limit to
+ *  two decimal places. Example: 1234567.2345 -> 1,234,567.23
+ **/
+function formatNr(x: number, nrDecimals: number = 0) {
+  return x.toFixed(nrDecimals).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function tableHeader(runs: string[]): string {
+  const models = runs.map((path) => path.substring(0, path.indexOf("/")));
+  const runNrs = runs.map((path) =>
+    path
+      .substring(path.lastIndexOf("run"), path.length - 1)
+      .replace("run", "run \\#")
+  );
+  return `
+{\\scriptsize
+\\begin{tabular}{l||rrrrrrrr|rrrrrrrr}
+ & \\multicolumn{8}{|c|}{\\it ${models[0]} (${runNrs[0]})} &   \\multicolumn{8}{|c}{\\it ${models[1]} (${runNrs[1]})} \\\\
+ & \\Candidates & \\Invalid & \\Identical & \\Duplicate & \\Total & \\Killed & \\Survived & \\Timeout 
+ & \\Candidates & \\Invalid & \\Identical & \\Duplicate & \\Total & \\Killed & \\Survived & \\Timeout \\\\
+\\hline`;
+}
+
+function generateDataBlock(baseDir: string, runs: string[]): string {
+  let latexTable = tableHeader(runs);
   for (const projectName of projectNames) {
     let row = "\\textit{" + projectName + "}";
     for (const run of runs) {
-      const data = fs.readFileSync(
-        path.join(baseDir, run, "projects", projectName, "StrykerInfo.json"),
-        "utf8"
+      const strykerInfo = JSON.parse(
+        fs.readFileSync(
+          path.join(baseDir, run, "projects", projectName, "StrykerInfo.json"),
+          "utf8"
+        )
       );
-      const info = JSON.parse(data);
-      const nrKilled = info.nrKilled;
-      const nrSurvived = info.nrSurvived;
-      const nrTimedout = info.nrTimedOut;
-      const total =
-        parseInt(nrKilled) + parseInt(nrSurvived) + parseInt(nrTimedout);
+      const llmorpheusInfo = JSON.parse(
+        fs.readFileSync(
+          path.join(baseDir, run, "projects", projectName, "summary.json"),
+          "utf8"
+        )
+      );
+      const nrCandidates = llmorpheusInfo.nrCandidates;
+      const nrSyntacticallyInvalid = llmorpheusInfo.nrSyntacticallyInvalid;
+      const nrIdentical = llmorpheusInfo.nrIdentical;
+      const nrDuplicate = llmorpheusInfo.nrDuplicate;
+      const nrKilled = parseInt(strykerInfo.nrKilled);
+      const nrSurvived = parseInt(strykerInfo.nrSurvived);
+      const nrTimedout = parseInt(strykerInfo.nrTimedOut);
+      const nrMutants: number = nrKilled + nrSurvived + nrTimedout;
+
       row +=
         " & " +
-        total +
+        formatNr(nrCandidates) +
         " & " +
-        nrKilled +
+        formatNr(nrSyntacticallyInvalid) +
         " & " +
-        nrSurvived +
+        formatNr(nrIdentical) +
         " & " +
-        nrTimedout;
+        formatNr(nrDuplicate) +
+        " & " +
+        formatNr(nrMutants) +
+        " & " +
+        formatNr(nrKilled) +
+        " & " +
+        formatNr(nrSurvived) +
+        " & " +
+        formatNr(nrTimedout);
     }
     row += " \\\\\n";
     latexTable += row;
   }
-  latexTable +=
-    "\\end{tabular}\n" +
-    "}\n" +
-    "\\caption{Comparison of mutants generated using the \\CodeLlamaThirtyFour, \\CodeLlamaThirteen, and \\Mixtral LLMs at temperature 0.0}\n" +
-    "\\label{table:CompareLLMs}\n" +
-    "\\end{table*}\n";
-  console.log(latexTable);
+  let totalRow: string = "\\hline\\textit{Total}";
+  let totalCandidates;
+  let totalInvalid;
+  let totalIdentical;
+  let totalDuplicate;
+  let totalMutants;
+  let totalKilled;
+  let totalSurvived;
+  let totalTimedout;
+  for (const run of runs) {
+    totalCandidates = 0;
+    totalInvalid = 0;
+    totalIdentical = 0;
+    totalDuplicate = 0;
+    totalMutants = 0;
+    totalKilled = 0;
+    totalSurvived = 0;
+    totalTimedout = 0;
+    for (const projectName of projectNames) {
+      const strykerInfo = JSON.parse(
+        fs.readFileSync(
+          path.join(baseDir, run, "projects", projectName, "StrykerInfo.json"),
+          "utf8"
+        )
+      );
+      const llmorpheusInfo = JSON.parse(
+        fs.readFileSync(
+          path.join(baseDir, run, "projects", projectName, "summary.json"),
+          "utf8"
+        )
+      );
+      totalCandidates += parseInt(llmorpheusInfo.nrCandidates);
+      totalInvalid += parseInt(llmorpheusInfo.nrSyntacticallyInvalid);
+      totalIdentical += parseInt(llmorpheusInfo.nrIdentical);
+      totalDuplicate += parseInt(llmorpheusInfo.nrDuplicate);
+      totalKilled += parseInt(strykerInfo.nrKilled);
+      totalSurvived += parseInt(strykerInfo.nrSurvived);
+      totalTimedout += parseInt(strykerInfo.nrTimedOut);
+    }
+    totalMutants =
+      totalCandidates - totalInvalid - totalIdentical - totalDuplicate;
+    totalRow +=
+      " & " +
+      formatNr(totalCandidates) +
+      " & " +
+      formatNr(totalInvalid) +
+      " & " +
+      formatNr(totalIdentical) +
+      " & " +
+      formatNr(totalDuplicate) +
+      " & " +
+      formatNr(totalMutants) +
+      " & " +
+      formatNr(totalKilled) +
+      " & " +
+      formatNr(totalSurvived) +
+      " & " +
+      formatNr(totalTimedout);
+  }
+  latexTable += totalRow + " \\\\\n";
+  latexTable += "\\end{tabular}\n" + "}\n";
+  return latexTable;
 }
 
-// usage: node benchmark/compareLLMs.js <baseDir> <list of subdirs>
+/**
+ * Generate a LaTeX table that shows the number of mutants generated
+ * at different temperatures.
+ */
+function generateTable(baseDir: string, runs: string[]): string {
+  let latexTable = `
+% table generated using command: "node benchmark/compareLLMs.js ${baseDir.substring(
+    baseDir.lastIndexOf("/") + 1
+  )}${runs.join(" ")}
+\\begin{table*}
+\\centering`;
+
+  latexTable += generateDataBlock(baseDir, runs.slice(0, 2));
+  latexTable += "\\\\[5mm]";
+  latexTable += generateDataBlock(baseDir, runs.slice(2, 4));
+
+  latexTable += `
+  \\vspace*{3mm}
+  \\caption{Mutants generated with the \\CodeLlamaThirteen, \\Mixtral, \\LlamaThreeThree, and \\GPTFouroMini LLMs, using
+  the following parameters:
+   temperature: 0.0, 
+    maxTokens: 250, 
+    maxNrPrompts: 2000, 
+    template: \\textit{template-full.hb}, 
+    systemPrompt: \\textit{SystemPrompt-MutationTestingExpert.txt}, 
+    rateLimit: 0, 
+    nrAttempts: 3. 
+  }
+ \\label{table:CompareLLMs}
+ \\end{table*}`;
+  return latexTable;
+}
+
+// usage: node benchmark/compareLLMs.js <baseDir> <list of 4 subdirs>
 
 const baseDir = process.argv[2];
 const runs = process.argv.slice(3);
-generateTable(baseDir, runs);
+const table = generateTable(baseDir, runs);
+console.log(table);
