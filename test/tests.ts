cb2d254aad3a4462615e88d731c59a9cd133c234
franktip <f.tip@northeastern.edu>
2026-07-29 12:39:30 -0400

    remove dependence on ReplayModel in tests

--- test/tests.ts
+++ test/tests.ts
@@ -2,7 +2,6 @@ import fs from "fs";
 import path from "path";
 
 import { FixtureModel } from "../src/model/FixtureModel";
-import { ReplayModel } from "../src/model/ReplayModel";
 // import { Model } from "../src/model/Model"; // uncomment when adding a new live-LLM test
 import { PromptSpecGenerator } from "../src/generator/PromptSpecGenerator";
 import { MutantGenerator } from "../src/generator/MutantGenerator";
@@ -291,166 +290,20 @@ describe("test mutant generation", () => {
   });
 
   it("should replay a previously observed execution", async () => {
-    const dirContainingRecording = "./test/input/recorded/sorters/";
-    const model = new ReplayModel(dirContainingRecording);
+    const model = new FixtureModel(modelName, mockModelDir);
     const outputDir = fs.mkdtempSync(path.join(".", "test-"));
     const metaInfo: MetaInfo = {
-      modelName: "codellama-34b-instruct",
-      template: "templates/template-full.hb",
-      systemPrompt: "SystemPrompt-MutationTestingExpert.txt",
+      modelName: modelName,
+      template: promptTemplateFileName,
+      systemPrompt: "",
       maxTokens: 250,
       temperature: 0,
-      maxNrPrompts: 1250,
-      nrAttempts: 3,
+      maxNrPrompts: 100,
+      nrAttempts: 1,
       timeout: 10_000,
       mutate: "src/**/TreeSorter.ts",
-      ignore: "**/*.spec.ts",
-      rateLimit: 0,
-      mutateOnly: undefined,
-      mutateOnlyLines: undefined,
-      maxLinesInPlaceHolder: 1,
-    };
-    const mutantGenerator = new MutantGenerator(
-      model,
-      outputDir,
-      "test/input/testProject/sorters",
-      metaInfo
-    );
-    await mutantGenerator.generateMutants();
-    const filePath = path.join(
-      outputDir,
-      "template-full_codellama-34b-instruct_0.0",
-      "summary.json"
-    );
-    const actualSummaryJson: any = JSON.parse(
-      fs.readFileSync(path.join(filePath), "utf8")
-    );
-    const expectedSummaryJson: any = JSON.parse(
-      fs.readFileSync("./test/input/recorded/sorters/summary.json", "utf8")
-    );
-
-    // check that the summary file contains the right information. Note that we don't know
-    // the number of tokens used during replay, so we can't check that.
-    expect(actualSummaryJson.nrPrompts).to.equal(expectedSummaryJson.nrPrompts);
-    expect(actualSummaryJson.nrCandidates).to.equal(
-      expectedSummaryJson.nrCandidates
-    );
-    expect(actualSummaryJson.nrSyntacticallyValid).to.equal(
-      expectedSummaryJson.nrSyntacticallyValid
-    );
-    expect(actualSummaryJson.nrSyntacticallyInvalid).to.equal(
-      expectedSummaryJson.nrSyntacticallyInvalid
-    );
-    expect(actualSummaryJson.nrSemanticallyInvalid).to.equal(
-      expectedSummaryJson.nrSemanticallyInvalid
-    );
-    expect(actualSummaryJson.nrIdentical).to.equal(
-      expectedSummaryJson.nrIdentical
-    );
-    expect(actualSummaryJson.nrDuplicate).to.equal(
-      expectedSummaryJson.nrDuplicate
-    );
-    expect(actualSummaryJson.nrLocations).to.equal(
-      expectedSummaryJson.nrLocations
-    );
-
-    expect(actualSummaryJson.metaInfo.modelName).to.equal(
-      expectedSummaryJson.metaInfo.modelName
-    );
-    expect(actualSummaryJson.metaInfo.temperature).to.equal(
-      expectedSummaryJson.metaInfo.temperature
-    );
-    expect(actualSummaryJson.metaInfo.maxTokens).to.equal(
-      expectedSummaryJson.metaInfo.maxTokens
-    );
-    expect(actualSummaryJson.metaInfo.maxNrPrompts).to.equal(
-      expectedSummaryJson.metaInfo.maxNrPrompts
-    );
-    expect(actualSummaryJson.metaInfo.rateLimit).to.equal(
-      expectedSummaryJson.metaInfo.rateLimit
-    );
-    expect(actualSummaryJson.metaInfo.nrAttempts).to.equal(
-      expectedSummaryJson.metaInfo.nrAttempts
-    );
-    expect(actualSummaryJson.metaInfo.template).to.equal(
-      expectedSummaryJson.metaInfo.template
-    );
-    expect(actualSummaryJson.metaInfo.systemPrompt).to.equal(
-      expectedSummaryJson.metaInfo.systemPrompt
-    );
-    expect(actualSummaryJson.metaInfo.mutate).to.equal(
-      expectedSummaryJson.metaInfo.mutate
-    );
-    expect(actualSummaryJson.metaInfo.ignore).to.equal(
-      expectedSummaryJson.metaInfo.ignore
-    );
-
-    // now check that the actual prompt and completion files are the same as the expected ones
-    const actualFiles = fs.readdirSync(
-      path.join(
-        outputDir,
-        "template-full_codellama-34b-instruct_0.0",
-        "prompts"
-      )
-    );
-    const expectedFiles = fs.readdirSync(
-      path.join(dirContainingRecording, "prompts")
-    );
-    expect(actualFiles.length).to.equal(expectedFiles.length);
-
-    // check that the same prompt and completion files are in both directories
-    const inActualButNotInExpected = actualFiles.filter(
-      (fileName) => !expectedFiles.includes(fileName)
-    );
-    expect(
-      inActualButNotInExpected,
-      `expected ${inActualButNotInExpected.join(",")} to be empty`
-    ).to.be.empty;
-    const inExpectedButNotInActual = expectedFiles.filter(
-      (fileName) => !actualFiles.includes(fileName)
-    );
-    expect(
-      inExpectedButNotInActual,
-      `expected ${inExpectedButNotInActual.join(",")} to be empty`
-    ).to.be.empty;
-
-    // check that the file contents match
-    for (const actualFileName of actualFiles) {
-      const actualFileContents = fs.readFileSync(
-        path.join(
-          outputDir,
-          "template-full_codellama-34b-instruct_0.0",
-          "prompts",
-          actualFileName
-        ),
-        "utf8"
-      );
-      const expectedFileContents = fs.readFileSync(
-        path.join(dirContainingRecording, "prompts", actualFileName),
-        "utf8"
-      );
-      expect(actualFileContents).to.equal(expectedFileContents);
-    }
-
-    fs.rmdirSync(outputDir, { recursive: true });
-  });
-
-  it("should apply mutations to a single line", async () => {
-    const dirContainingRecording = "./test/input/recorded/sorters/";
-    const model = new ReplayModel(dirContainingRecording);
-    const outputDir = fs.mkdtempSync(path.join(".", "test-"));
-    const metaInfo: MetaInfo = {
-      modelName: "codellama-34b-instruct",
-      template: "templates/template-full.hb",
-      systemPrompt: "SystemPrompt-MutationTestingExpert.txt",
-      maxTokens: 250,
-      temperature: 0,
-      maxNrPrompts: 1250,
-      nrAttempts: 3,
-      timeout: 10_000,
-      mutate: "src/**/TreeSorter.ts",
-      ignore: "**/*.spec.ts",
-      rateLimit: 0,
+      ignore: "src/**/*.spec.ts",
+      rateLimit: 1000,
       mutateOnly: undefined,
       mutateOnlyLines: [9],
       maxLinesInPlaceHolder: 1,
@@ -458,147 +311,44 @@ describe("test mutant generation", () => {
     const mutantGenerator = new MutantGenerator(
       model,
       outputDir,
-      "test/input/testProject/sorters",
+      sorterProjectPath,
       metaInfo
     );
     await mutantGenerator.generateMutants();
-    const filePath = path.join(
-      outputDir,
-      "template-full_codellama-34b-instruct_0.0",
-      "summary.json"
-    );
     const actualSummaryJson: any = JSON.parse(
-      fs.readFileSync(path.join(filePath), "utf8")
+      fs.readFileSync(
+        path.join(outputDir, subDirName, "summary.json"),
+        "utf8"
+      )
     );
     const expectedSummaryJson: any = JSON.parse(
-      fs.readFileSync(
-        "./test/input/recorded/sorters/summary-line-9-only.json",
-        "utf8"
-      )
+      fs.readFileSync("./test/input/recorded/sorters/summary-line-9-only.json", "utf8")
     );
-
-    // check that the summary file contains the right information. Note that we don't know
-    // the number of tokens used during replay, so we can't check that.
     expect(actualSummaryJson.nrPrompts).to.equal(expectedSummaryJson.nrPrompts);
-    expect(actualSummaryJson.nrCandidates).to.equal(
-      expectedSummaryJson.nrCandidates
-    );
-    expect(actualSummaryJson.nrSyntacticallyValid).to.equal(
-      expectedSummaryJson.nrSyntacticallyValid
-    );
-    expect(actualSummaryJson.nrSyntacticallyInvalid).to.equal(
-      expectedSummaryJson.nrSyntacticallyInvalid
-    );
-    expect(actualSummaryJson.nrSemanticallyInvalid).to.equal(
-      expectedSummaryJson.nrSemanticallyInvalid
-    );
-    expect(actualSummaryJson.nrIdentical).to.equal(
-      expectedSummaryJson.nrIdentical
-    );
-    expect(actualSummaryJson.nrDuplicate).to.equal(
-      expectedSummaryJson.nrDuplicate
-    );
-    expect(actualSummaryJson.nrLocations).to.equal(
-      expectedSummaryJson.nrLocations
-    );
-
-    expect(actualSummaryJson.metaInfo.modelName).to.equal(
-      expectedSummaryJson.metaInfo.modelName
-    );
-    expect(actualSummaryJson.metaInfo.temperature).to.equal(
-      expectedSummaryJson.metaInfo.temperature
-    );
-    expect(actualSummaryJson.metaInfo.maxTokens).to.equal(
-      expectedSummaryJson.metaInfo.maxTokens
-    );
-    expect(actualSummaryJson.metaInfo.maxNrPrompts).to.equal(
-      expectedSummaryJson.metaInfo.maxNrPrompts
-    );
-    expect(actualSummaryJson.metaInfo.rateLimit).to.equal(
-      expectedSummaryJson.metaInfo.rateLimit
-    );
-    expect(actualSummaryJson.metaInfo.nrAttempts).to.equal(
-      expectedSummaryJson.metaInfo.nrAttempts
-    );
-    expect(actualSummaryJson.metaInfo.template).to.equal(
-      expectedSummaryJson.metaInfo.template
-    );
-    expect(actualSummaryJson.metaInfo.systemPrompt).to.equal(
-      expectedSummaryJson.metaInfo.systemPrompt
-    );
-    expect(actualSummaryJson.metaInfo.mutate).to.equal(
-      expectedSummaryJson.metaInfo.mutate
-    );
-    expect(actualSummaryJson.metaInfo.ignore).to.equal(
-      expectedSummaryJson.metaInfo.ignore
-    );
-
-    // now check that the actual prompt and completion files are the same as the expected ones
-    const actualFiles = fs.readdirSync(
-      path.join(
-        outputDir,
-        "template-full_codellama-34b-instruct_0.0",
-        "prompts"
-      )
-    );
-    const expectedFiles = fs.readdirSync(
-      path.join(dirContainingRecording, "prompts-line-9-only")
-    );
-    expect(actualFiles.length).to.equal(expectedFiles.length);
-
-    // check that the same prompt and completion files are in both directories
-    const inActualButNotInExpected = actualFiles.filter(
-      (fileName) => !expectedFiles.includes(fileName)
-    );
-    expect(
-      inActualButNotInExpected,
-      `expected ${inActualButNotInExpected.join(",")} to be empty`
-    ).to.be.empty;
-    const inExpectedButNotInActual = expectedFiles.filter(
-      (fileName) => !actualFiles.includes(fileName)
-    );
-    expect(
-      inExpectedButNotInActual,
-      `expected ${inExpectedButNotInActual.join(",")} to be empty`
-    ).to.be.empty;
-
-    // check that the file contents match
-    for (const actualFileName of actualFiles) {
-      const actualFileContents = fs.readFileSync(
-        path.join(
-          outputDir,
-          "template-full_codellama-34b-instruct_0.0",
-          "prompts",
-          actualFileName
-        ),
-        "utf8"
-      );
-      const expectedFileContents = fs.readFileSync(
-        path.join(dirContainingRecording, "prompts", actualFileName),
-        "utf8"
-      );
-      expect(actualFileContents).to.equal(expectedFileContents);
-    }
-
+    expect(actualSummaryJson.nrCandidates).to.equal(expectedSummaryJson.nrCandidates);
+    expect(actualSummaryJson.nrSyntacticallyValid).to.equal(expectedSummaryJson.nrSyntacticallyValid);
+    expect(actualSummaryJson.nrSyntacticallyInvalid).to.equal(expectedSummaryJson.nrSyntacticallyInvalid);
+    expect(actualSummaryJson.nrIdentical).to.equal(expectedSummaryJson.nrIdentical);
+    expect(actualSummaryJson.nrDuplicate).to.equal(expectedSummaryJson.nrDuplicate);
+    expect(actualSummaryJson.nrLocations).to.equal(expectedSummaryJson.nrLocations);
     fs.rmdirSync(outputDir, { recursive: true });
   });
 
-  it('should apply mutations mutations only to lines containing "<"', async () => {
-    const dirContainingRecording = "./test/input/recorded/sorters/";
-    const model = new ReplayModel(dirContainingRecording);
+  it('should apply mutations only to lines containing "<"', async () => {
+    const model = new FixtureModel(modelName, mockModelDir);
     const outputDir = fs.mkdtempSync(path.join(".", "test-"));
     const metaInfo: MetaInfo = {
-      modelName: "codellama-34b-instruct",
-      template: "templates/template-full.hb",
-      systemPrompt: "SystemPrompt-MutationTestingExpert.txt",
+      modelName: modelName,
+      template: promptTemplateFileName,
+      systemPrompt: "",
       maxTokens: 250,
       temperature: 0,
-      maxNrPrompts: 1250,
-      nrAttempts: 3,
+      maxNrPrompts: 100,
+      nrAttempts: 1,
       timeout: 10_000,
       mutate: "src/**/TreeSorter.ts",
-      ignore: "**/*.spec.ts",
-      rateLimit: 0,
+      ignore: "src/**/*.spec.ts",
+      rateLimit: 1000,
       mutateOnly: "<",
       mutateOnlyLines: undefined,
       maxLinesInPlaceHolder: 1,
@@ -606,132 +356,26 @@ describe("test mutant generation", () => {
     const mutantGenerator = new MutantGenerator(
       model,
       outputDir,
-      "test/input/testProject/sorters",
+      sorterProjectPath,
       metaInfo
     );
     await mutantGenerator.generateMutants();
-    const filePath = path.join(
-      outputDir,
-      "template-full_codellama-34b-instruct_0.0",
-      "summary.json"
-    );
     const actualSummaryJson: any = JSON.parse(
-      fs.readFileSync(path.join(filePath), "utf8")
+      fs.readFileSync(
+        path.join(outputDir, subDirName, "summary.json"),
+        "utf8"
+      )
     );
     const expectedSummaryJson: any = JSON.parse(
-      fs.readFileSync(
-        "./test/input/recorded/sorters/summary-lessthan-only.json",
-        "utf8"
-      )
+      fs.readFileSync("./test/input/recorded/sorters/summary-lessthan-only.json", "utf8")
     );
-
-    // check that the summary file contains the right information. Note that we don't know
-    // the number of tokens used during replay, so we can't check that.
     expect(actualSummaryJson.nrPrompts).to.equal(expectedSummaryJson.nrPrompts);
-    expect(actualSummaryJson.nrCandidates).to.equal(
-      expectedSummaryJson.nrCandidates
-    );
-    expect(actualSummaryJson.nrSyntacticallyValid).to.equal(
-      expectedSummaryJson.nrSyntacticallyValid
-    );
-    expect(actualSummaryJson.nrSyntacticallyInvalid).to.equal(
-      expectedSummaryJson.nrSyntacticallyInvalid
-    );
-    expect(actualSummaryJson.nrSemanticallyInvalid).to.equal(
-      expectedSummaryJson.nrSemanticallyInvalid
-    );
-    expect(actualSummaryJson.nrIdentical).to.equal(
-      expectedSummaryJson.nrIdentical
-    );
-    expect(actualSummaryJson.nrDuplicate).to.equal(
-      expectedSummaryJson.nrDuplicate
-    );
-    expect(actualSummaryJson.nrLocations).to.equal(
-      expectedSummaryJson.nrLocations
-    );
-
-    expect(actualSummaryJson.metaInfo.modelName).to.equal(
-      expectedSummaryJson.metaInfo.modelName
-    );
-    expect(actualSummaryJson.metaInfo.temperature).to.equal(
-      expectedSummaryJson.metaInfo.temperature
-    );
-    expect(actualSummaryJson.metaInfo.maxTokens).to.equal(
-      expectedSummaryJson.metaInfo.maxTokens
-    );
-    expect(actualSummaryJson.metaInfo.maxNrPrompts).to.equal(
-      expectedSummaryJson.metaInfo.maxNrPrompts
-    );
-    expect(actualSummaryJson.metaInfo.rateLimit).to.equal(
-      expectedSummaryJson.metaInfo.rateLimit
-    );
-    expect(actualSummaryJson.metaInfo.nrAttempts).to.equal(
-      expectedSummaryJson.metaInfo.nrAttempts
-    );
-    expect(actualSummaryJson.metaInfo.template).to.equal(
-      expectedSummaryJson.metaInfo.template
-    );
-    expect(actualSummaryJson.metaInfo.systemPrompt).to.equal(
-      expectedSummaryJson.metaInfo.systemPrompt
-    );
-    expect(actualSummaryJson.metaInfo.mutate).to.equal(
-      expectedSummaryJson.metaInfo.mutate
-    );
-    expect(actualSummaryJson.metaInfo.ignore).to.equal(
-      expectedSummaryJson.metaInfo.ignore
-    );
-
-    // now check that the actual prompt and completion files are the same as the expected ones
-    const actualFiles = fs.readdirSync(
-      path.join(
-        outputDir,
-        "template-full_codellama-34b-instruct_0.0",
-        "prompts"
-      )
-    );
-    const expectedFiles = fs.readdirSync(
-      path.join(dirContainingRecording, "prompts-lessthan-only")
-    );
-    expect(actualFiles.length).to.equal(expectedFiles.length);
-
-    // check that the same prompt and completion files are in both directories
-    const inActualButNotInExpected = actualFiles.filter(
-      (fileName) => !expectedFiles.includes(fileName)
-    );
-    expect(
-      inActualButNotInExpected,
-      `expected ${inActualButNotInExpected.join(",")} to be empty`
-    ).to.be.empty;
-    const inExpectedButNotInActual = expectedFiles.filter(
-      (fileName) => !actualFiles.includes(fileName)
-    );
-    expect(
-      inExpectedButNotInActual,
-      `expected ${inExpectedButNotInActual.join(",")} to be empty`
-    ).to.be.empty;
-
-    // check that the file contents match
-    for (const actualFileName of actualFiles) {
-      const actualFileContents = fs.readFileSync(
-        path.join(
-          outputDir,
-          "template-full_codellama-34b-instruct_0.0",
-          "prompts",
-          actualFileName
-        ),
-        "utf8"
-      );
-      const expectedFileContents = fs.readFileSync(
-        path.join(
-          dirContainingRecording,
-          "prompts-lessthan-only",
-          actualFileName
-        ),
-        "utf8"
-      );
-      expect(actualFileContents).to.equal(expectedFileContents);
-    }
-
+    expect(actualSummaryJson.nrCandidates).to.equal(expectedSummaryJson.nrCandidates);
+    expect(actualSummaryJson.nrSyntacticallyValid).to.equal(expectedSummaryJson.nrSyntacticallyValid);
+    expect(actualSummaryJson.nrSyntacticallyInvalid).to.equal(expectedSummaryJson.nrSyntacticallyInvalid);
+    expect(actualSummaryJson.nrIdentical).to.equal(expectedSummaryJson.nrIdentical);
+    expect(actualSummaryJson.nrDuplicate).to.equal(expectedSummaryJson.nrDuplicate);
+    expect(actualSummaryJson.nrLocations).to.equal(expectedSummaryJson.nrLocations);
     fs.rmdirSync(outputDir, { recursive: true });
   });
 });
